8fb8002750299a7c0435dffb2b0cc66d4b335238
Dmitry <dmitry@deimdal.com>
2025-09-10 21:51:18 +0200

    Fix tests

--- Deimdal.JsonSchemaConvert.Tests/ConversionTests.cs
+++ Deimdal.JsonSchemaConvert.Tests/ConversionTests.cs
@@ -6,18 +6,19 @@ namespace Deimdal.JsonSchemaConvert.Tests;
 public class ConversionTests
 {
     [Theory]
-    [InlineData("Schemas\\petstore.json", "Schemas\\petstore_converted_2019_09.json", SchemaVersion.Draft2019_09)]
-    [InlineData("Schemas\\petstore.json", "Schemas\\petstore_converted_2020_12.json", SchemaVersion.Draft2020_12)]
-    [InlineData("Schemas\\petstore.json", "Schemas\\petstore_converted_7.json", SchemaVersion.Draft7)]
-    [InlineData("Schemas\\petstore.json", "Schemas\\petstore_converted_2019_09.yaml", SchemaVersion.Draft2019_09)]
-    [InlineData("Schemas\\petstore.json", "Schemas\\petstore_converted_2020_12.yaml", SchemaVersion.Draft2020_12)]
-    [InlineData("Schemas\\petstore.json", "Schemas\\petstore_converted_7.yaml", SchemaVersion.Draft7)]
+    [InlineData("petstore.json", "petstore_converted_2019_09.json", SchemaVersion.Draft2019_09)]
+    [InlineData("petstore.json", "petstore_converted_2020_12.json", SchemaVersion.Draft2020_12)]
+    [InlineData("petstore.json", "petstore_converted_7.json", SchemaVersion.Draft7)]
+    [InlineData("petstore.json", "petstore_converted_2019_09.yaml", SchemaVersion.Draft2019_09)]
+    [InlineData("petstore.json", "petstore_converted_2020_12.yaml", SchemaVersion.Draft2020_12)]
+    [InlineData("petstore.json", "petstore_converted_7.yaml", SchemaVersion.Draft7)]
     public async Task ConvertJsonSuccess(string input, string output, SchemaVersion version)
     {
-        var actual = await Converter.ConvertSchema(input, version);
+        var sourcePath = Path.Combine(AppContext.BaseDirectory, "Schemas", input);
+        var actual = await Converter.ConvertSchema(sourcePath, version);
 
-        var path = Path.Combine(AppContext.BaseDirectory, output);
-        var expected = await File.ReadAllTextAsync(path);
+        var pathActual = Path.Combine(AppContext.BaseDirectory, "Schemas", output);
+        var expected = await File.ReadAllTextAsync(pathActual);
         Assert.Equal(expected, actual);
     }
 }
